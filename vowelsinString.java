18f0755be15194f0012c0bf950a398a9c901e709
Sahil sanjay more <108298194+sahilprojgith@users.noreply.github.com>
2025-08-12 22:29:53 +0530

    Add files via upload

---
+++ vowelsinString.java
@@ -0,0 +1,25 @@
+package faqs;
+
+public class vowelsinString {
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        str = str.toLowerCase();
+
+        int count = 0;
+
+        for(int i = 0;i<str.length();i++){
+            char ch =str.charAt(i);
+
+            if(ch =='a' ||ch =='e' ||ch =='i'
+                    ||ch =='o' ||ch =='u'){
+                count++;
+            }
+        }
+
+        System.out.println("Number of vowels" + count);
+
+
+
+    }
+}
