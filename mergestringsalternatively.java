2d8ef904d1e9308ef1c9af18690d37ad54e2803d
Sahil sanjay more <108298194+sahilprojgith@users.noreply.github.com>
2025-08-31 05:11:39 +0530

    Add files via upload

---
+++ mergestringsalternatively.java
@@ -0,0 +1,34 @@
+package faqs;
+
+
+//Leetcode 1768
+public class mergestringsalternatively {
+
+    public static String mergeStrings(String word1, String word2){
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+        int j = 0;
+
+        while(i< word1.length() || j<word2.length()){
+            if(i< word1.length()){
+                result.append(word1.charAt(i));
+                i++;
+            }
+            if(j< word2.length()){
+                result.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mergeStrings("abc", "pqr"));
+        System.out.println(mergeStrings("ab", "pqrs"));
+
+    }
+
+
+
+}
