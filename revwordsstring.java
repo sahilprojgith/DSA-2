2d79fbf10a1b0a4f5a710a789cd2d8fd11a9314a
Sahil sanjay more <108298194+sahilprojgith@users.noreply.github.com>
2025-09-11 23:56:07 +0530

    Add files via upload

---
+++ revwordsstring.java
@@ -0,0 +1,35 @@
+package faqs;
+
+public class revwordsstring {
+
+    public String reverseWords(String s) {
+        s = s.trim();
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = s.length() - 1;
+        int j = i;
+
+        while (i >= 0) {
+            while (i >= 0 && s.charAt(i) != ' ') {
+                i--;
+            }
+
+        stringBuilder.append(s.substring(i + 1, j + 1)).append(" ");
+
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        j = i;
+
+
+    }
+        return stringBuilder.toString().trim();
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
